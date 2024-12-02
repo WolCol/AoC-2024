0705070bd35267b29099d66f466d10b71769c2b9
WolCol <168994586+WolCol@users.noreply.github.com>
2024-12-02 16:04:57 +0000

    finish d1p1

--- src/main/java/day01/Day01Part01.java
+++ src/main/java/day01/Day01Part01.java
@@ -1,12 +1,37 @@
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Day01Part01 {
     public static void main(String[] args) {
         String dateiPfad = "/Users/wolfs/Documents/GitHub/AoC-2024/src/main/resources/day01.txt";
         List<String> zeilen = InputReader.readInputByLine(dateiPfad);
+        List<Integer> leftNums = new ArrayList<>();
+        List<Integer> rightNums = new ArrayList<>();
 
-        for (String zeile : zeilen) {
-            System.out.println("Zeile: " + zeile);
+        /*for (int i = 0; i < zeilen.size(); i++) {
+            System.out.println("Zeile " + (i + 1) + ": " + zeilen.get(i));
+        }*/
+
+        for (int i = 0; i < zeilen.size(); i++) {
+            String zeile = zeilen.get(i);
+            String[] parts = zeile.split("\\s+");
+            leftNums.add(Integer.parseInt(parts[0]));
+            rightNums.add(Integer.parseInt(parts[1]));
         }
+        Collections.sort(leftNums);
+        Collections.sort(rightNums);
+
+        int counter = 0;
+        int[] result = new int[zeilen.size()];
+        int finalcalc = 0;
+        while (counter < leftNums.size() && counter <rightNums.size()) {
+            //System.out.println(leftNums.get(counter) + " " + rightNums.get(counter));
+            result[counter] = Math.abs(rightNums.get(counter) - leftNums.get(counter));
+            finalcalc += result[counter];
+            //System.out.println(result[counter]);
+            counter++;
+        }
+        System.out.println("The number is: "+finalcalc);
     }
 }
